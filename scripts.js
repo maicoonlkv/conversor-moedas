c7c39b38137407a2351de741dd2975bc43806c3c
maicoonlkv <maicon.kvictor@gmail.com>
2024-01-17 22:44:09 -0300

    ADICIONADO MOEDAS DE ORIGEM PARA CONVERSAO

--- scripts.js
+++ scripts.js
@@ -1,4 +1,5 @@
 const converterButton = document.getElementById("converterButton")
+const currencySource = document.querySelector(".currencySource")
 const currencySelect = document.querySelector(".currencySelect")
 
 
@@ -13,11 +14,7 @@ function converterValues() {  //Criacao da função
     const libraToday = 6.26
     const bitcoinToday = 210552.05
 
-
-
-
-
-
+    
     if (currencySelect.value == "dolar") {
         currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US", {
             style: "currency", //estilo que quero mudar, no caso é uma moeda
@@ -64,10 +61,45 @@ function converterValues() {  //Criacao da função
 
 }
 
+function changeCurrencyOrigin() {
+    const currencyOptionNameOrigin = document.querySelector(".currency-text") //pega a classe do valor do HTML para mudar 
+    const currencyImgOrigin = document.querySelector(".currencyImgOrigin") //pega a classe da imagem do HTML
+
+    if(currencySource.value == "deReal") {
+        currencyOptionNameOrigin.innerHTML = "Real Brasileiro" //muda escrita para Dolar americano
+        currencyImgOrigin.src = "./assets/real.png" //muda imagem para bandeira do euro
+    }
+    
+    if(currencySource.value == "deDolar") {
+        currencyOptionNameOrigin.innerHTML = "Dolar Americano" //muda escrita para Dolar americano
+        currencyImgOrigin.src = "./assets/dolar.png" //muda imagem para bandeira do euro
+    }
+
+    if(currencySource.value == "deEuro") {
+        currencyOptionNameOrigin.innerHTML = "Euro" //muda escrita para Euro
+        currencyImgOrigin.src = "./assets/euro.png" //muda imagem para bandeira do euro
+    }
+
+    if(currencySource.value == "deLibra") {
+        currencyOptionNameOrigin.innerHTML = "Libra" //muda escrita para Euro
+        currencyImgOrigin.src = "./assets/libra.png" //muda imagem para bandeira do euro
+    }
+
+    if(currencySource.value == "deBitcoin") {
+        currencyOptionNameOrigin.innerHTML = "Bitcoin" //muda escrita para Euro
+        currencyImgOrigin.src = "./assets/bitcoin.png" //muda imagem para bandeira do euro
+    }
+}
+
 function changeCurrency() {
     const currencyOption = document.querySelector(".currency-option") //pega a classe do valor do HTML para mudar 
     const currencyImg = document.querySelector(".currencyImg") //pega a classe da imagem do HTML
 
+    if(currencySelect.value == "real") {
+        currencyOption.innerHTML = "Real Brasileiro" //muda escrita para Dolar americano
+        currencyImg.src = "./assets/real.png" //muda imagem para bandeira do euro
+    }
+    
     if(currencySelect.value == "dolar") {
         currencyOption.innerHTML = "Dolar Americano" //muda escrita para Dolar americano
         currencyImg.src = "./assets/dolar.png" //muda imagem para bandeira do euro
@@ -87,8 +119,11 @@ function changeCurrency() {
         currencyOption.innerHTML = "Bitcoin" //muda escrita para Euro
         currencyImg.src = "./assets/bitcoin.png" //muda imagem para bandeira do euro
     }
-    
 }
 
+
+
+
+currencySource.addEventListener("change", changeCurrencyOrigin)
 currencySelect.addEventListener("change", changeCurrency) //valida quando troco a moeda
 converterButton.addEventListener("click", converterValues) //valida se o click esta pegando, dpois de criar a variavel
